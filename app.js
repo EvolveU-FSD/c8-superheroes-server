43b456c2ead58f4c61c7dddc8044a8c31ad39058
Danielle Barker - Mac <caseybarker86@gmail.com>
2022-05-27 09:37:25 -0600

    added react files to serve from express

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const session = require("express-session");
 const passport = require("passport");
+const path = require("path");
 const superheroRouter = require("./routes/superheroRoutes");
 const authRouter = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(logger("dev"));
@@ -13,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(session({ secret: "apple" }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/superheroes", superheroRouter);
 app.use("/auth", authRouter);
 module.exports = app;
